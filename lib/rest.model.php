a472ad9065d950e1b5d69cdb60bd9b4e812add4e
Vinoth Kumar <vinoth@rigpa.in>
2020-03-30 16:41:46 +0530

    Bug Fixes

--- lib/rest.model.php
+++ lib/rest.model.php
@@ -521,10 +521,11 @@
             try {
                 $Dbobj = new DbConnection();
                 $conn = $Dbobj->getdbconnect();
-                $query = mysqli_query($conn, "SELECT qc.id, qc.user_id, u.role, u.name as user_name, comments, created_on as date_time FROM quotation_comments qc INNER JOIN users u ON qc.user_id = u.id WHERE qc.quotation_id = ".$id);
+                $query = mysqli_query($conn, "SELECT qc.id, qc.user_id, u.role, u.name as user_name, qc.comments, qc.created_on as date_time FROM quotation_comments qc INNER JOIN users u ON qc.user_id = u.id WHERE qc.quotation_id = ".$id);
                 $count  = mysqli_num_rows($query);
                 if ($count > 0) {
                     while($row = mysqli_fetch_assoc($query)) {
+                        $row['date_time'] = $this->time_elapsed_string($row['date_time']);
                         $comments[] = $row;
                     }
                 }
@@ -534,6 +535,36 @@
             }
             return $comments;
         }
+
+        function time_elapsed_string($datetime, $full = false) {
+            $now = new DateTime;
+            $ago = new DateTime($datetime);
+            $diff = $now->diff($ago);
+        
+            $diff->w = floor($diff->d / 7);
+            $diff->d -= $diff->w * 7;
+        
+            $string = array(
+                'y' => 'year',
+                'm' => 'month',
+                'w' => 'week',
+                'd' => 'day',
+                'h' => 'hour',
+                'i' => 'minute',
+                's' => 'second',
+            );
+            foreach ($string as $k => &$v) {
+                if ($diff->$k) {
+                    $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+                } else {
+                    unset($string[$k]);
+                }
+            }
+        
+            if (!$full) $string = array_slice($string, 0, 1);
+            return $string ? implode(', ', $string) . ' ago' : 'just now';
+        }
+        
 
     }
 ?>
