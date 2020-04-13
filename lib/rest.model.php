0c9bf9c7e2b94001328f311b0e12a5a32b81f4b4
Vinoth Kumar <vinoth@rigpa.in>
2020-04-13 12:49:16 +0530

    API changes

--- lib/rest.model.php
+++ lib/rest.model.php
@@ -338,7 +338,7 @@
             try {
                 $Dbobj = new DbConnection();
                 $conn = $Dbobj->getdbconnect();
-                $query = mysqli_query($conn, "SELECT COUNT(*) as notifications_count FROM notifications WHERE recipient_id = '".$recipient_id);
+                $query = mysqli_query($conn, "SELECT COUNT(*) as notifications_count FROM notifications WHERE recipient_id = '".$recipient_id."'");
                 $count  = mysqli_num_rows($query);
                 if ($count > 0) {
                     while($row = mysqli_fetch_assoc($query)) {
