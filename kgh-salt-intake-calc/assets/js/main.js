c467d84c4cc7d76d8c485b1fd43354654f0aae3d
Vinoth Kumar <vinoth@rigpa.in>
2020-05-09 20:25:46 +0530

    kgh html included

--- kgh-salt-intake-calc/assets/js/main.js
+++ kgh-salt-intake-calc/assets/js/main.js
@@ -39,7 +39,7 @@
 	});
 	
 	$(document).ready(function(){
-
+		
 		$('.submit').click(function(){
 			validateForm();   
 		});
@@ -167,54 +167,21 @@
 			return false;
 		});
 		
-		function validateForm(){
-		
-			var nameReg = /^[A-Za-z]+$/;
-			var numberReg =  /^[0-9]+$/;
-			var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-		
-			var names = $('#nameInput').val();
-			var company = $('#companyInput').val();
-			var email = $('#emailInput').val();
-			var telephone = $('#telInput').val();
-			var message = $('#messageInput').val();
-		
-			var inputVal = new Array(names, company, email, telephone, message);
-		
-			var inputMessage = new Array("name", "company", "email address", "telephone number", "message");
-		
-			 $('.error').hide();
-		
-				if(inputVal[0] == ""){
-					$('#nameLabel').after('<span class="error"> Please enter your ' + inputMessage[0] + '</span>');
-				} 
-				else if(!nameReg.test(names)){
-					$('#nameLabel').after('<span class="error"> Letters only</span>');
-				}
-		
-				if(inputVal[1] == ""){
-					$('#companyLabel').after('<span class="error"> Please enter your ' + inputMessage[1] + '</span>');
-				}
-		
-				if(inputVal[2] == ""){
-					$('#emailLabel').after('<span class="error"> Please enter your ' + inputMessage[2] + '</span>');
-				} 
-				else if(!emailReg.test(email)){
-					$('#emailLabel').after('<span class="error"> Please enter a valid email address</span>');
-				}
-		
-				if(inputVal[3] == ""){
-					$('#telephoneLabel').after('<span class="error"> Please enter your ' + inputMessage[3] + '</span>');
-				} 
-				else if(!numberReg.test(telephone)){
-					$('#telephoneLabel').after('<span class="error"> Numbers only</span>');
-				}
-		
-				if(inputVal[4] == ""){
-					$('#messageLabel').after('<span class="error"> Please enter your ' + inputMessage[4] + '</span>');
-				}       
-		}   
-		
 		});
 
 })(jQuery);
+function calculateBMI() {
+			
+	var height = $.trim($('#height').val());
+	var weight = $.trim($('#weight').val());
+	if (height > 0 && weight > 0) {
+	  var bmi;
+	  var heightInM2 = (height * height);
+	  console.log("heightInM2", heightInM2);
+	  bmi = (weight / heightInM2).toFixed(2);
+	  console.log("calculateBMI", bmi);
+	  $('#bmi').val(bmi);
+	} else {
+		$('#bmi').val("");
+	}
+  }
