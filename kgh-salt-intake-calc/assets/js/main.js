a42eeb5565bf993a08a080a556030b458887140a
Vinoth Kumar <vinoth@rigpa.in>
2020-05-13 21:47:57 +0530

    logo changes

--- kgh-salt-intake-calc/assets/js/main.js
+++ kgh-salt-intake-calc/assets/js/main.js
@@ -39,6 +39,46 @@
 	});
 
 	$(document).ready(function () {
+		$("#nav ul li a[href^='#']").on('click', function(e) {
+
+			// prevent default anchor click behavior
+			e.preventDefault();
+		 
+			// store hash
+			var hash = this.hash;
+		 
+			// animate
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			  }, 1000, function(){
+		 
+				// when done, add hash to url
+				// (default click behaviour)
+				window.location.hash = hash;
+			  });
+		 
+		 });
+		 
+		 $("#nav li a[href^='#']").on('click', function(e) {
+		 
+			// prevent default anchor click behavior
+			e.preventDefault();
+		 
+			// store hash
+			var hash = this.hash;
+		 
+			// animate
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			  }, 1000, function(){
+		 
+				// when done, add hash to url
+				// (default click behaviour)
+				window.location.hash = hash;
+			  });
+		 
+		 });
+
 		$(window).scroll(function(){
 			if ($(window).scrollTop() >= 300) {
 				$('nav').addClass('fixed-header');
